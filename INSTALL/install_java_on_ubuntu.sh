5a853019521164458a26fccc74c95f785e6642f0
Ubuntu <ubuntu@ip-172-31-39-117.us-east-2.compute.internal>
2019-10-03 21:40:27 +0000

    Added support for Unbuntu

---
+++ INSTALL/install_java_on_ubuntu.sh
@@ -0,0 +1,9 @@
+#Install Java
+sudo apt-get install default-jdk
+
+
+#Modify bashrc to add the new java home                                                                                                                                
+echo 'JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/' >> ~/.bashrc
+echo 'export JAVA_HOME' >> ~/.bashrc
+echo 'PATH=$JAVA_HOME/bin:$PATH' >> ~/.bashrc
+echo 'export PATH=/usr/local/bin:$PATH' >>~/.bashrc
